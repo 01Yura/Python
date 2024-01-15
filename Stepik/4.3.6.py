1d711eac887eb7f807488e799ee6e0edbb8e26b5
Yura Primyshev <p020885@yandex.ru>
2024-01-15 15:14:38 -0500

    2

---
+++ Stepik/4.3.6.py
@@ -0,0 +1,31 @@
+color1=input()
+color2=input()
+if color1=='красный':
+    if color2=='синий':
+        print('фиолетовый')
+    elif color2=='желтый':
+        print('оранжевый')
+    elif color2=='красный':
+        print('красный')
+    else:
+        print('ошибка цвета')
+elif color1=='синий':
+    if color2=='желтый':
+        print('зеленый')
+    elif color2=='желтый':
+        print('оранжевый')
+    elif color2=='синий':
+        print('синий')
+    else:
+        print('ошибка цвета')
+elif color1=='желтый':
+    if color2=='синий':
+        print('зеленый')
+    elif color2=='красный':
+        print('оранжевый')
+    elif color2=='желтый':
+        print('желтый')
+    else:
+        print('ошибка цвета')
+else:
+    print('ошибка цвета')
