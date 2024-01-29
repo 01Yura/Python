c38f2cf4483141c4c504969b4606f3c271df53fa
Yura Primyshev <p020885@yandex.ru>
2024-01-28 21:43:17 -0500

    123

---
+++ Stepik/7.8.8.py
@@ -0,0 +1,11 @@
+# буз оптимизации, работает очень долго
+from math import pow
+
+for a in range(1,151):
+    for b in range(1,151):
+        for c in range(1,151):
+            for d in range(1,151):
+                for e in range(1,151):
+                    print(a,b,c,d,e)
+                    if pow(a,5)+pow(b,5)+pow(c,5)+pow(d,5)==pow(e,5):
+                    print(a+b+c+d+e)
