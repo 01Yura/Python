44d1c4cfb01868bc4e07e62fb65f482f3f1034ec
Yura Primyshev <p020885@yandex.ru>
2024-01-30 12:51:54 -0500

    1

--- Stepik/7.8.8.py
+++ Stepik/7.8.8.py
@@ -8,4 +8,4 @@ for a in range(1,151):
                 for e in range(1,151):
                     print(a,b,c,d,e)
                     if pow(a,5)+pow(b,5)+pow(c,5)+pow(d,5)==pow(e,5):
-                    print(a+b+c+d+e)
+                        print(a+b+c+d+e)
