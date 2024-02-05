f6b441394822ae0a453abab893292631682aecb8
Yura Primyshev <p020885@yandex.ru>
2024-02-04 23:18:59 -0500

    123

--- Stepik/7.9.3.py
+++ Stepik/7.9.3.py
@@ -1,12 +1,11 @@
 a, b = int(input()), int(input())
-summa = 0
+maxtotal=0
 for i in range(a, b + 1):
-    summa1 = summa
-    summa = 0
-    for j in range(1, i + 1):
-        if i % j == 0:
-            summa += j
-    if summa1 <= summa:
-        maxsumma = summa
-        maxdigit = i
-print(maxdigit, maxsumma)
+    total=0
+    for divisor in range (1, i+1):
+        if i%divisor==0:
+            total+=divisor
+        if total>=maxtotal:
+            maxtotal=total
+            digit=i
+print(digit, maxtotal)
