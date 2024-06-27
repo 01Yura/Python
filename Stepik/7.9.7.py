18f474e76784a963d411243d1be6d5cbb84e9f73
Yura Primyshev <p020885@yandex.ru>
2024-06-26 20:49:07 -0400

    1

---
+++ Stepik/7.9.7.py
@@ -0,0 +1,13 @@
+a,b=int(input()),int(input())
+total=0
+for i in range(a,b+1):
+    a=i
+    while i<=b+1:
+        if a%1=0:
+    total+=1
+    i+=1
+    if total>1:
+        break
+    else:
+        print(i)
+    print()
